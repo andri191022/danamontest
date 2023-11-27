05d540ef229b0197bcf29489e851d1c1ab7c19d3
andri191022 <andri1022@gmail.com>
2023-11-27 14:37:02 +0700

    s
    s

--- Testing.DanamonNew/Logic/GeneralLogic.cs
+++ Testing.DanamonNew/Logic/GeneralLogic.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Testing.DanamonNew.Utility;
 using static Testing.DanamonNew.Utility.SD;
 using Testing.DanamonNew.Models.Dto;
+using System.Security.Cryptography;
 
 
 namespace Testing.DanamonNew.Logic
@@ -22,14 +23,18 @@ namespace Testing.DanamonNew.Logic
                 case ApiType.PUT:
                     //relative url +bdi timestamp + secret key + additional key + request body
 
+
+
                     break;
                 case ApiType.DELETE:
                 case ApiType.GET:
-
                     //relative url +bdi timestamp + secret key + additional key
 
+
+
+
                     break;
-                default:                   
+                default:
                     break;
             }
 
@@ -40,19 +45,56 @@ namespace Testing.DanamonNew.Logic
 
         private string GenerateSHA256(string text)
         {
-            using (System.Security.Cryptography.SHA256 sha256Hash = System.Security.Cryptography.SHA256.Create())
+            using (SHA256 sha256 = SHA256.Create())
             {
-                // ComputeHash - returns byte array
-                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(text));
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
 
-                // Convert byte array to a string
                 StringBuilder builder = new StringBuilder();
-                for (int i = 0; i < bytes.Length; i++)
+                for (int i = 0; i < hashBytes.Length; i++)
                 {
-                    builder.Append(bytes[i].ToString("x2"));
+                    builder.Append(hashBytes[i].ToString("x2")); 
                 }
                 return builder.ToString();
             }
         }
-    }
+
+
+        private string GenerateSHA256withRSA(string data, RSAParameters privateKey)
+        {
+            byte[] resultData; string result = string.Empty;
+            using (var rsa = RSA.Create())
+            {
+                rsa.ImportParameters(privateKey);
+
+                byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] hashedData = sha256.ComputeHash(dataBytes);
+                    resultData = rsa.SignHash(hashedData, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+                }
+            }
+
+            return Convert.ToBase64String(resultData);
+        }
+
+
+        private string GenerateHMAC_SHA512(string message, string key)
+        {
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key);
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            using (HMACSHA512 hmac = new HMACSHA512(keyBytes))
+            {
+                byte[] hashBytes = hmac.ComputeHash(messageBytes);
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    builder.Append(hashBytes[i].ToString("x2")); // Convert each byte to its hexadecimal representation
+                }
+                return builder.ToString();
+            }
+
+        }
 }
